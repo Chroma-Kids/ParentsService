c6b30b5df50ea4ad20c3560510885145544986ec
MiguelCG <miguelcg1101@gmail.com>
2018-02-25 13:18:45 +0000

    Add basic CRUD operations

---
+++ src/main/java/com/chromakids/parentsservice/controller/ParentController.java
@@ -0,0 +1,71 @@
+package com.chromakids.parentsservice.controller;
+
+import com.chromakids.parentsservice.model.Parent;
+import com.chromakids.parentsservice.repository.ParentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/parents")
+public class ParentController {
+
+  @Autowired
+  ParentRepository parentRepository;
+
+  @GetMapping("")
+  public List<Parent> getAllParents() {
+    return parentRepository.findAll();
+  }
+
+  @PostMapping("")
+  public Parent createParent(@Valid @RequestBody Parent parent) {
+    return parentRepository.save(parent);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Parent> getParentById(@PathVariable(value = "id") Long parentId) {
+    Parent parent = parentRepository.findOne(parentId);
+    if (parent == null) {
+      return ResponseEntity.notFound().build();
+    }
+    return ResponseEntity.ok(parent);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Parent> updateParent(@PathVariable(value = "id") Long parentId, @Valid @RequestBody Parent parentDetails) {
+    Parent parent = parentRepository.findOne(parentId);
+    if (parent == null) {
+      return ResponseEntity.notFound().build();
+    }
+    parent.setName(parentDetails.getName());
+    parent.setSurname(parentDetails.getSurname());
+    parent.setAddress(parentDetails.getAddress());
+
+    Parent updatedNote = parentRepository.save(parent);
+    return ResponseEntity.ok(updatedNote);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Parent> deleteNote(@PathVariable(value = "id") Long parentId) {
+    Parent parent = parentRepository.findOne(parentId);
+    if (parent == null) {
+      return ResponseEntity.notFound().build();
+    }
+
+    parentRepository.delete(parent);
+    return ResponseEntity.ok().build();
+  }
+
+}
---
+++ src/main/java/com/chromakids/parentsservice/model/Parent.java
@@ -0,0 +1,95 @@
+package com.chromakids.parentsservice.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "parents")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
+public class Parent implements Serializable {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  @NotBlank
+  private String name;
+  @NotBlank
+  private String surname;
+  @NotBlank
+  private String address;
+
+  @Column(nullable = false, updatable = false)
+  @Temporal(TemporalType.TIMESTAMP)
+  @CreatedDate
+  private Date createdAt;
+
+  @Column(nullable = false)
+  @Temporal(TemporalType.TIMESTAMP)
+  @LastModifiedDate
+  private Date updatedAt;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getSurname() {
+    return surname;
+  }
+
+  public void setSurname(String surname) {
+    this.surname = surname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public Date getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(Date createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public Date getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt(Date updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+}
